f480798038fc24a439c828dcb0571444697fa777
Esperlluette <janl.mannessier@gmail.com>
2024-04-14 13:27:42 +0000

    flowbite OK

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\ErrorCode;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,12 +11,22 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(EntityManagerInterface $em): Response
     {
+        $codes = $em->getRepository(ErrorCode::class)->findAll();
+        $http_codes = [];
 
-        
+        foreach ($codes as $key => $value) {
+            array_push($http_codes, $value->getCode());
+        }
+
+        /**
+         * le site utilise l'id de la base de données et non pas le code erreur pour la comparaison, l'url n'est donc pas conforme à la consigne 
+         * 
+         */
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'codes' => $http_codes,
+
         ]);
     }
 }
